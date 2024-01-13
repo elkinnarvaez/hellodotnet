9c149ac3a61f2ad7312c6172ed2ccd35fa5a7bab
Elkin Narvaez <elkinnarvaez2406@gmail.com>
2024-01-13 14:52:55 -0500

    Change branch

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World Chiquito!");
+app.MapGet("/", () => "Hello Worlds!");
 
 app.Run();
